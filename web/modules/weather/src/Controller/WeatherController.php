d731d508695bbb943cd869f4313b025e014423f5
Oumar FALL <oumarfall@hotmail.fr>
2020-12-16 19:37:21 +0100

    for now the /weather page display a dump message

--- web/modules/weather/src/Controller/WeatherController.php
+++ web/modules/weather/src/Controller/WeatherController.php
@@ -12,14 +12,23 @@ class WeatherController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build() {
+  public function build(string $city, string $country_code) {
 
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->_fetch($city, $country_code),
     ];
 
     return $build;
   }
 
+  private function _fetch($city, $country = null) {
+
+    return "The weather in " . $city . " (" . $country . ") is ...";
+  // if is_null($country):
+  
+  // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  // api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+  // api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+ }
 }
