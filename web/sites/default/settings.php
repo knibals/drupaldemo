a51c6716a8208d65edd0e03c3e74b81ec655272b
Oumar FALL <oumarfall@hotmail.fr>
2021-08-19 23:27:18 +0000

    modify the "trusted_host_pattern" to match the new URL

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -715,7 +715,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * example.org, with all subdomains included.
  */
 $settings['trusted_host_patterns'] = [
-     '^meteo\.local$',
+     '^drupaldemo\.local$',
 ];
 
 /**
