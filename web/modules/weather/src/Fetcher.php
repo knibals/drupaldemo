2abf4bc9e6f5d9b0153d2c400045209b4285104e
Oumar FALL <oumarfall@hotmail.fr>
2021-08-20 08:18:13 +0000

    bootstrap the "weather fetcher" Service

---
+++ web/modules/weather/src/Fetcher.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\weather;
+
+/**
+ * Weather fetcher service.
+ */
+class Fetcher {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
