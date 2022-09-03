1d32aa40bb6ef756fd3c2627cdf64c87e215f082
Oumar FALL <oumarfall@hotmail.fr>
2022-09-03 13:14:48 +0000

    create the Loti theme

---
+++ web/themes/loti/js/loti.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.loti = {
+    attach: function (context, settings) {
+      console.log('It works!');
+    }
+  };
+
+} (Drupal));
